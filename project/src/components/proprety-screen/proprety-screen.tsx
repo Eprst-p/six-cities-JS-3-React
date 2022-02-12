8f9d1bece35bc96360f34f495d1cb75c8659fb34
Eprst <easter_p@mail.ru>
2022-02-12 20:34:05 +0300

    img и photo из описания alt

--- project/src/components/proprety-screen/proprety-screen.tsx
+++ project/src/components/proprety-screen/proprety-screen.tsx
@@ -39,22 +39,22 @@ function PropretyScreen(): JSX.Element {
             <div className="property__gallery-container container">
               <div className="property__gallery">
                 <div className="property__image-wrapper">
-                  <img className="property__image" src="img/room.jpg" alt="Photo studio" />
+                  <img className="property__image" src="img/room.jpg" alt="studio" />
                 </div>
                 <div className="property__image-wrapper">
-                  <img className="property__image" src="img/apartment-01.jpg" alt="Photo studio" />
+                  <img className="property__image" src="img/apartment-01.jpg" alt="studio" />
                 </div>
                 <div className="property__image-wrapper">
-                  <img className="property__image" src="img/apartment-02.jpg" alt="Photo studio" />
+                  <img className="property__image" src="img/apartment-02.jpg" alt="studio" />
                 </div>
                 <div className="property__image-wrapper">
-                  <img className="property__image" src="img/apartment-03.jpg" alt="Photo studio" />
+                  <img className="property__image" src="img/apartment-03.jpg" alt="studio" />
                 </div>
                 <div className="property__image-wrapper">
-                  <img className="property__image" src="img/studio-01.jpg" alt="Photo studio" />
+                  <img className="property__image" src="img/studio-01.jpg" alt="studio" />
                 </div>
                 <div className="property__image-wrapper">
-                  <img className="property__image" src="img/apartment-01.jpg" alt="Photo studio" />
+                  <img className="property__image" src="img/apartment-01.jpg" alt="studio" />
                 </div>
               </div>
             </div>
@@ -237,7 +237,7 @@ function PropretyScreen(): JSX.Element {
                 <article className="near-places__card place-card">
                   <div className="near-places__image-wrapper place-card__image-wrapper">
                     <a href="#">
-                      <img className="place-card__image" src="img/room.jpg" width="260" height="200" alt="Place image" />
+                      <img className="place-card__image" src="img/room.jpg" width="260" height="200" alt="Place" />
                     </a>
                   </div>
                   <div className="place-card__info">
@@ -269,7 +269,7 @@ function PropretyScreen(): JSX.Element {
                 <article className="near-places__card place-card">
                   <div className="near-places__image-wrapper place-card__image-wrapper">
                     <a href="#">
-                      <img className="place-card__image" src="img/apartment-02.jpg" width="260" height="200" alt="Place image" />
+                      <img className="place-card__image" src="img/apartment-02.jpg" width="260" height="200" alt="Place" />
                     </a>
                   </div>
                   <div className="place-card__info">
@@ -304,7 +304,7 @@ function PropretyScreen(): JSX.Element {
                   </div>
                   <div className="near-places__image-wrapper place-card__image-wrapper">
                     <a href="#">
-                      <img className="place-card__image" src="img/apartment-03.jpg" width="260" height="200" alt="Place image" />
+                      <img className="place-card__image" src="img/apartment-03.jpg" width="260" height="200" alt="Place" />
                     </a>
                   </div>
                   <div className="place-card__info">
