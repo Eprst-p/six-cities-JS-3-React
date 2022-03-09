8e54359bedfff4344b05ec6e9182505e7af7f413
Eprst <easter_p@mail.ru>
2022-03-09 13:32:01 +0300

    sort элемент

--- project/src/components/main-screen/main-screen.tsx
+++ project/src/components/main-screen/main-screen.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
 import PlaceCard from './place-card';
+import SortForm from './sort-form';
 import MainMap from '../map/main-map';
 import {useAppDispatch, useAppSelector} from '../../hooks/redux-hooks';
 import {changeCity, chooseOfferID} from '../../store/action';
@@ -50,21 +51,7 @@ function MainScreen(): JSX.Element {
           <section className="cities__places places">
             <h2 className="visually-hidden">Places</h2>
             <b className="places__found">{`${offersForCity.length} places to stay in ${newCity}`}</b>
-            <form className="places__sorting" action="#" method="get">
-              <span className="places__sorting-caption">Sort by</span>
-              <span className="places__sorting-type" tabIndex={0}>
-                Popular
-                <svg className="places__sorting-arrow" width="7" height="4">
-                  <use xlinkHref="#icon-arrow-select"></use>
-                </svg>
-              </span>
-              <ul className="places__options places__options--custom places__options--opened">
-                <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-                <li className="places__option" tabIndex={0}>Price: low to high</li>
-                <li className="places__option" tabIndex={0}>Price: high to low</li>
-                <li className="places__option" tabIndex={0}>Top rated first</li>
-              </ul>
-            </form>
+              <SortForm />
             <div className="cities__places-list places__list tabs__content">
               {
                 offersForCity.map((location) => (
