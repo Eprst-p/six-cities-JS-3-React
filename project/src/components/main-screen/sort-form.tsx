468b9a6a8701dc6af2839e2e8077f6491b1c9560
Eprst <easter_p@mail.ru>
2022-03-12 09:31:43 +0300

    обработчик на сорт-элементе

--- project/src/components/main-screen/sort-form.tsx
+++ project/src/components/main-screen/sort-form.tsx
@@ -1,4 +1,6 @@
-import {useState} from 'react';
+/* eslint-disable no-console */
+import {useState, MouseEvent} from 'react';
+import {SortOptions} from '../../settings/sort-options';
 
 function SortForm(): JSX.Element {
   const [isOpened, setOpenedStatus] = useState(false);
@@ -6,22 +8,31 @@ function SortForm(): JSX.Element {
     setOpenedStatus(!isOpened);
  };
 
- return (
+  const [activeOption, setActiveOption] = useState(SortOptions.Popular.toString());//корявая toString, но иначе TS с типами ругается
+  const placesOptions = Array.from(Object.values(SortOptions));
+  const handlerOptionClick = (evt:MouseEvent<HTMLLIElement>) => {
+    setActiveOption(evt.currentTarget.id);
+  };
+
+  return (
     <form className="places__sorting" action="#" method="get" onClick = {handlerSortCLick}>
-    <span className="places__sorting-caption">Sort by</span>
-    <span className="places__sorting-type" tabIndex={0}>
-      Popular
-      <svg className="places__sorting-arrow" width="7" height="4">
-        <use xlinkHref="#icon-arrow-select"></use>
-      </svg>
-    </span>
-    <ul className={`places__options places__options--custom places__options${isOpened ? '--opened' : ''}`}>
-      <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-      <li className="places__option" tabIndex={0}>Price: low to high</li>
-      <li className="places__option" tabIndex={0}>Price: high to low</li>
-      <li className="places__option" tabIndex={0}>Top rated first</li>
-    </ul>
-  </form>
+      <span className="places__sorting-caption">Sort by </span>
+      <span className="places__sorting-type" tabIndex={0}>
+        {activeOption}
+        <svg className="places__sorting-arrow" width="7" height="4">
+          <use xlinkHref="#icon-arrow-select"></use>
+        </svg>
+      </span>
+      <ul className={`places__options places__options--custom places__options${isOpened ? '--opened' : ''}`}>
+        {
+          placesOptions.map((option) =>
+            (
+              <li id={option} className={`places__option ${activeOption === option ? 'places__option--active' : ''} `} tabIndex={0} onClick={handlerOptionClick} key={option}>{option}</li>
+            ),
+          )
+        }
+      </ul>
+    </form>
   );
 }
 
